c9f35023e75efd65460866d61b6a1c87e26efcc8
lianwangtao <wlian3@wisc.edu>
2016-04-03 14:30:21 -0500

    push to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
-http.createServer(function(request, response) {
-  console.log("created server");
-}).listen(3000);
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "app/index.html";
+  } else {
+    filePath = "app" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+var port_number = server.listen(process.env.PORT || 3000);
