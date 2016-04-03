de87ae50ca27f27a47dc85032dcb2e95a7a6cfc0
lianwangtao <wlian3@wisc.edu>
2016-04-03 14:32:07 -0500

    push to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
 var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
-
 
 var server = http.createServer(function(request, response) {
   var filePath = false;
