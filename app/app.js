70352ef0a575e52735a932b59ef7d2760278ba16
lianwangtao <wlian3@wisc.edu>
2016-04-03 15:08:41 -0500

    push to heroku

--- app/app.js
+++ app/app.js
@@ -19,7 +19,7 @@ app.controller('resultListCtrl', ['$scope','$http', function($scope, $http) {
   }).then(function successCallback(response) {
     // this callback will be called asynchronously
     // when the response is available
-    $scope.result = response;
+    $scope.results = response;
   }, function errorCallback(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
